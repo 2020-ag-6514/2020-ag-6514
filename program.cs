f1d61502567f14792c3c9728bd1e49cc90eda493
Dhanak <ifzayousaf2@gmail.com>
2021-11-25 10:40:51 -0800

    first commit

---
+++ program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OOPExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Console.WriteLine(i = 10);
+            Console.ReadLine();
+
+        }
+    }
+}
